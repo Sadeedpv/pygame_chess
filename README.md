18cd8c89850510f699089f08c8c89ae611248602
Sadeed pv <muhammedsadeed70@gmail.com>
2023-08-06 15:21:51 +0530

    Add move logics

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-This is a simple chess game made with Pygame.
+This is a chess game made with pygame.
 
-To run the game, open a terminal window and navigate to the directory where you saved the files. Then, type the following command:
+To run the game, open a terminal window and navigate to the directory where the code is saved. Then type the following command:
 
 ```
 python main.py
@@ -10,9 +10,10 @@ The game will start and you can play it by clicking on the squares on the chessb
 
 Here are some explanations of the code:
 
-* The `pygame` module is used to create the graphics and handle the user input.
-* The `chessBoard` variable is a 2D list that represents the chessboard. Each element in the list is a string that represents the piece that is on that square.
+* The `pygame` library is used to create the graphics and handle the user input.
+* The `chessboard` is a 2D list that represents the chessboard. Each element of the list is a string that represents the piece that is on that square.
 * The `draw_board()` function draws the chessboard on the screen.
 * The `handleClick()` function handles the logic for when the user clicks on a square on the chessboard.
+* The `get_moves()` function returns a list of all the possible moves for a given piece.
 
 I hope this helps!
--- utils/functions.py
+++ utils/functions.py
@@ -1,38 +1,82 @@
 
 
 def get_moves(piece_name, row, col, chessBoard, board_size):
+
+    """
+    Possible moves for a Pawn
+    """
     if piece_name.endswith("pawn"):
         print("It's a Pawn")
         moves = []
         if piece_name.startswith("white"):
             if row == 6:
-                moves.append((row - 1, col))
-                moves.append((row - 2, col))
+                moves.append(check_isNone(chessBoard, row - 1, col))
+                moves.append(check_isNone(chessBoard, row - 2, col))
             else:
-                moves.append((row - 1, col))
+                moves.append(check_isNone(chessBoard, row - 1, col))
         else:
             if row == 1:
-                moves.append((row + 1, col))
-                moves.append((row + 2, col))
+                moves.append(check_isNone(chessBoard, row + 1, col))
+                moves.append(check_isNone(chessBoard, row + 2, col))
             else:
-                moves.append((row + 1, col))
+                moves.append(check_isNone(chessBoard, row + 1, col))
         
         return moves
 
+    """
+    Possible moves for a Knight
+    """
+
     if piece_name.endswith("knight"):
         print("It's a Knight")
+        moves = []
+        moves.append(check_isNone(chessBoard, row - 2, col - 1))
+        moves.append(check_isNone(chessBoard, row - 2, col + 1))
+        moves.append(check_isNone(chessBoard, row - 1, col - 2))
+        moves.append(check_isNone(chessBoard, row - 1, col + 2))
+        moves.append(check_isNone(chessBoard, row + 1, col - 2))
+        moves.append(check_isNone(chessBoard, row + 1, col + 2))
+        moves.append(check_isNone(chessBoard, row + 2, col - 1))
+        moves.append(check_isNone(chessBoard, row + 2, col + 1))
+        return moves
     
+    """
+    Possible moves for a Bishop
+    """
     if piece_name.endswith("bishop"):
         print("It's a Bishop")
         moves = []
         # Check possible moves in diagonal direction (up and down)
-        for i in range(board_size):
-            moves.append((row + i, col + i))
-            moves.append((row - i, col - i))
-            moves.append((row + i, col - i))
-            moves.append((row - i, col + i))
+        for i in range(1,board_size):
+            if row + i < board_size and col + i < board_size:
+                if chessBoard[row+i][col+i] is None:
+                    moves.append((row + i, col + i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row - i >= 0 and col - i >= 0:
+                if chessBoard[row-i][col-i] is None:
+                    moves.append((row - i, col - i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row+i < board_size and col - i >= 0:
+                if chessBoard[row+i][col-i] is None:
+                    moves.append((row + i, col - i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row-i >= 0 and col + i < board_size:
+                if chessBoard[row-i][col+i] is None:
+                    moves.append((row - i, col + i))
+                else:
+                    break
         return moves
 
+
+    """
+    Possible moves for a Queen
+    """
     if piece_name.endswith("queen"):
         print("It's a Queen")
         moves = []
@@ -61,26 +105,54 @@ def get_moves(piece_name, row, col, chessBoard, board_size):
                 moves.append((row, j))
             else:
                 break
-                # Check possible moves in diagonal direction (up and down)
-        for i in range(board_size):
-            moves.append((row + i, col + i))
-            moves.append((row - i, col - i))
-            moves.append((row + i, col - i))
-            moves.append((row - i, col + i))
+
+        # Check possible moves in diagonal direction (up and down)
+        for i in range(1,board_size):
+            if row + i < board_size and col + i < board_size:
+                if chessBoard[row+i][col+i] is None:
+                    moves.append((row + i, col + i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row - i >= 0 and col - i >= 0:
+                if chessBoard[row-i][col-i] is None:
+                    moves.append((row - i, col - i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row+i < board_size and col - i >= 0:
+                if chessBoard[row+i][col-i] is None:
+                    moves.append((row + i, col - i))
+                else:
+                    break
+        for i in range(1, board_size):
+            if row-i >= 0 and col + i < board_size:
+                if chessBoard[row-i][col+i] is None:
+                    moves.append((row - i, col + i))
+                else:
+                    break
         return moves
     
+    """
+    Possible moves for a King
+    """
     if piece_name.endswith("king"):
         print("It's a King")
         moves = []
-        moves.append((row - 1, col - 1))
-        moves.append((row - 1, col))
-        moves.append((row - 1, col + 1))
-        moves.append((row,col-1))
-        moves.append((row,col+1))
-        moves.append((row + 1, col - 1))
-        moves.append((row + 1, col))
-        moves.append((row + 1, col + 1))
+        moves.append(check_isNone(chessBoard, row - 1, col - 1))
+        moves.append(check_isNone(chessBoard, row - 1, col))
+        moves.append(check_isNone(chessBoard,row - 1, col + 1))
+        moves.append(check_isNone(chessBoard, row, col-1))
+        moves.append(check_isNone(chessBoard, row,col+1))
+        moves.append(check_isNone(chessBoard, row + 1, col - 1))
+        moves.append(check_isNone(chessBoard, row + 1, col))
+        moves.append(check_isNone(chessBoard, row + 1, col + 1))
         return moves
+
+
+    """
+    Possible moves for a Rook
+    """
 
     if piece_name.endswith("rook"):
         print("It's a Rook")
@@ -112,3 +184,14 @@ def get_moves(piece_name, row, col, chessBoard, board_size):
             else:
                 break
         return moves
+    
+
+def check_isNone(chessBoard, row, col):
+    try:
+        if chessBoard[row][col] is None:
+            return row,col
+        else:
+            return False
+    # If the index is out of range return False
+    except IndexError:
+        return False
